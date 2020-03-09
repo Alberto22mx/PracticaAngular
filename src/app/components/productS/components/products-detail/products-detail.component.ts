b48c45c0d869202c2baeb5c455febc2b43028680
Jose Alberto <alberto22mx@gmail.com>
2020-03-09 09:44:42 -0600

    Se actualza

--- src/app/components/productS/components/products-detail/products-detail.component.ts
+++ src/app/components/productS/components/products-detail/products-detail.component.ts
@@ -36,15 +36,36 @@ export class ProductsDetailComponent implements OnInit {
 
   createProduct() {
     const newProduct: Product = {
-      id: '25',
-      title: 'nuevo',
-      image: 'assets/images/banner-1.jpg',
-      price: 3000,
+      id: '3',
+      title: 'Mug',
+      image: 'assets/images/mug.png',
+      price: 7000,
       description: 'Nuevo producto'
     };
     this.productsService.createProduct(newProduct).subscribe(product => {
       console.log(product);
     });
   }
+  updateProduct() {
+    const updateProduct: Partial<Product> = {
+    id: '1',
+    image: 'assets/images/camiseta.png',
+    title: 'Camiseta',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+    };
+    this.productsService.updateProduct('2', updateProduct).subscribe(product => {
+    console.log(product);
+    });
+  }
+
+  deleteProduct(){
+    this.productsService.deleteProduct('1').subscribe(product => {
+    console.log(product);
+    });
+  }
 
 }
+
+
+//{"id":"1","image":"assets/images/camiseta.png","title":"Camiseta","price":80000,"description":"bla bla bla bla bla"}{"id":"2","image":"assets/images/hoodie.png","title":"Hoodie","price":5555,"description":"edicion titulo 2"},{"id":"4","title":"Pin","image":"assets/images/pin.png","price":7000,"description":"Nuevo producto"}
